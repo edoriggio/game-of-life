1669bf19b74657e7b28d72d998821bfdaeb96241
Edoardo Riggio <edo.riggio19@gmail.com>
2020-05-06 15:52:06 +0200

    added classes

---
+++ src/main/java/TextUserInterface.java
@@ -0,0 +1,26 @@
+package src.main.java;
+
+/**
+ * ...
+ */
+public class TextUserInterface {
+
+    private final Model model;
+
+
+    /**
+     * Create a new TUI operating on the given Model.
+     * @param model The model of this application
+     */
+    public TextUserInterface(final Model model) {
+        this.model = model;
+    }
+
+    /**
+     * Run the application.
+     */
+    public void run() {
+        System.out.println("Welcome to " + model.toString());
+    }
+
+}
