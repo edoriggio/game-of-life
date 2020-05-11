a288011d85b861a682cf44d9bf9bc3ec1f5cf125
Edoardo Riggio <edo.riggio19@gmail.com>
2020-05-11 23:00:52 +0200

    slightly changed file structure

--- src/test/GridTest.java
+++ src/test/GridTest.java
@@ -1,10 +1,6 @@
-package test;
+package src.test;
 
-import src.main.java.Cell;
-import src.main.java.Grid;
 import org.junit.Test;
-
-import static junit.framework.TestCase.*;
 
 public class GridTest {
 
