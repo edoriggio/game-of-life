b9a1f7689547762bd6e87e895edb32da31706d81
Michele Cattaneo <michelecattaneo18@gmail.com>
2020-05-13 11:00:45 +0200

    started test for MooreNeighborhood

---
+++ src/test/java/MooreNeighborhoodTest.java
@@ -0,0 +1,40 @@
+package src.test.java;
+
+import org.junit.Test;
+import src.main.java.*;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import static org.junit.Assert.*;
+
+// Note that this Neighborhood Rules gives the positions in this order:
+// North, South, East, West, North-West, North-east, South-West, South-east
+public class MooreNeighborhoodTest {
+
+    @Test
+    public void testCornerPosition() {
+        BorderRule borderRule = new TorusRule();
+        Neighborhood moore = new MooreNeighborhood(borderRule);
+        Grid grid = new Grid(10,20);
+        // get all neighbors of the position (0,0) with a more law and a torus law ( border )
+        LinkedList<int[]> neighbors = moore.getNeighbors(grid, 0, 0);
+
+        // North
+        assertTrue(Arrays.equals(neighbors.get(0), new int[]{9, 0}));
+        // South
+        assertTrue(Arrays.equals(neighbors.get(1), new int[]{1, 0}));
+        // East
+        assertTrue(Arrays.equals(neighbors.get(2), new int[]{0, 1}));
+        // West
+        assertTrue(Arrays.equals(neighbors.get(3), new int[]{0, 19}));
+        // North-West
+        assertTrue(Arrays.equals(neighbors.get(4), new int[]{9, 19}));
+        // North-east
+        assertTrue(Arrays.equals(neighbors.get(5), new int[]{9, 1}));
+        // South-west
+        assertTrue(Arrays.equals(neighbors.get(6), new int[]{1, 19}));
+        // South-east
+        assertTrue(Arrays.equals(neighbors.get(7), new int[]{1, 1}));
+    }
+}
