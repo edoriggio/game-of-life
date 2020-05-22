5985bd14a9287312763a927522f9630f302aae14
Michele Cattaneo <michelecattaneo18@gmail.com>
2020-05-22 12:06:02 +0200

    maven checkstyle fix

--- src/main/java/MainFrame.java
+++ src/main/java/MainFrame.java
@@ -4,10 +4,14 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Label;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-import javax.swing.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  * This class is responsible of creating the JFrame on which
