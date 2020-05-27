4d76f4ffc302ab04072088df72e9cbfbeba9d3fd
Michele Cattaneo <michelecattaneo18@gmail.com>
2020-05-27 15:56:09 +0200

    made code lighter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,20 +12,21 @@ import java.util.Scanner;
  */
 public class Main {
 
+    private static int rows = 80;
+    private static int cols = 80;
+    private static int cellSize = 10;
+
     private Main() {
         // Not to be instantiated
     }
 
     /**
      * Run the application.
-     * @param args the command line arguments in that order: number of rows, number of columns, cell size ( max 20px )
+     * @param args the command line arguments in that order:
+     *             number of rows, number of columns, cell size ( max 20px )
      * @throws InterruptedException Exception for the step timer
      */
     public static void main(final String[] args) throws InterruptedException {
-
-        int rows = 80;
-        int cols = 80;
-        int cellSize = 10;
 
         // Get terminal inputs:
         if (args.length == 3) {
@@ -35,29 +36,9 @@ public class Main {
         }
         // If no inputs were given, ask for them again:
         else {
-            Scanner scanner = new Scanner(System.in);
-            ArrayList<Integer> inputs = new ArrayList<>();
-            System.out.println("Provide parameters in that order: number of rows, number of columns, cell size");
-
-            int i = 0;
-            while (i < 3) {
-                if (scanner.hasNextInt()) {
-                    inputs.add(scanner.nextInt());
-
-                }
-                i++;
-            }
-            // If the given inputs and their number were valid
-            if (inputs.size() == 3) {
-                rows = inputs.get(0);
-                cols = inputs.get(1);
-                cellSize = inputs.get(2);
-            }
-            // If inputs were not valid, use the default ones defined at the beginning and notify in the standard output.
-            else {
+            if (!readInputs()){
                 System.out.println("Your inputs seems to be invalid; default values were given: rows="+rows+" cols="+cols+" cell size="+cellSize);
             }
-
         }
         // Make sure that the size of the cell is at most 20px
         cellSize = cellSize > 20 ? 20 : cellSize;
@@ -72,6 +53,32 @@ public class Main {
         // start executing steps of the instance of GameOfLife
         game.startExecutingSteps();
 
+    }
+
+    /**
+     * Read values from the standard input.
+     * @return true if the input and the number of inputs was valid, false otherwise.
+     */
+    private static boolean readInputs() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> inputs = new ArrayList<>();
+        System.out.println("Provide parameters in that order: number of rows, number of columns, cell size");
+
+        int i = 0;
+        while (i < 3) {
+            if (scanner.hasNextInt()) {
+                inputs.add(scanner.nextInt());
+
+            }
+            i++;
+        }
+        if (inputs.size() == 3) {
+            rows = inputs.get(0);
+            cols = inputs.get(1);
+            cellSize = inputs.get(2);
+            return true;
+        }
+        return false;
     }
 
 }
